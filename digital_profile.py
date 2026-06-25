7fd505d9a016b9c91907eaea14cdcd385cd2a97c
415-30-phuimpat <56820@yupparaj.ac.th>
2026-06-25 09:08:30 +0700

    Create  digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,10 @@
+import streamlit as ts
+st.title( MyDigtal Profile")
+st.header(แนะนําตัว)
+st.write(นางสาวใจดี วงงาม")
+st.write("ชื่อเล่น ใจดี")
+st.write("เลขที่19 ม .4/16")
+st.header("เป้าหมายและสิ่งที่อยากทําในอนาคต") 
+st.write("เป้าหมาย:**อยากเรียนด้านคอมพิวเตอร์และเทคโนโลยี")
+st.write("สิ่งที่อยากเป็นในอนาคต: นักธรุกิจ (businessperson)")
+         
